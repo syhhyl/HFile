43d456a3816ace9c46e805d5098c7b4072faf3ee
syhhyl <syhhyl926@126.com>
2026-02-10 11:01:11 +0800

    goto CLOSE_SOCK

--- src/server.c
+++ src/server.c
@@ -22,8 +22,7 @@ int server(char *path, uint16_t port) {
   int opt = 1;
   if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
     perror("setsockopt(SO_REUSEADDR)");
-    close(sock);
-    return 1;
+    goto CLOSE_SOCK;
   }
   
   struct sockaddr_in addr;
@@ -32,16 +31,12 @@ int server(char *path, uint16_t port) {
   addr.sin_addr.s_addr = htonl(INADDR_ANY);
   if (bind(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
     perror("bind");
-    // close(sock);
     goto CLOSE_SOCK;
-    return 1;
   }
   
   if (listen(sock, 1) == -1) {
     perror("listen");
-    // close(sock);
     goto CLOSE_SOCK;
-    return 1;
   }
   
 
@@ -66,12 +61,10 @@ int server(char *path, uint16_t port) {
     printf("n:%ld, sizeof(file_len):%zu\n", n, sizeof(file_len));
     if (n == 0) {
       // Client connected and closed immediately.
-      // close(conn);
       goto CLOSE_CONN;
     }
     if (n != 2) {
       perror("recv(file_len)");
-      // close(conn);
       goto CLOSE_CONN;
     }
     
@@ -82,25 +75,19 @@ int server(char *path, uint16_t port) {
     //get file_name
     if (file_len == 0 || file_len > 255) {
       fprintf(stderr, "invalid file name length: %u\n", (unsigned)file_len);
-      // close(conn);
       goto CLOSE_CONN;
-      continue;
     }
 
     char *file_name = (char *)malloc((size_t)file_len + 1);
     if (file_name == NULL) {
       perror("malloc(file_name)");
-      // close(conn);
       goto CLOSE_CONN;
-      continue;
     }
     ssize_t name_n = recv(conn, file_name, file_len, MSG_WAITALL);
     if (name_n != (ssize_t)file_len) {
       perror("recv(file_name)");
       free(file_name);
-      // close(conn);
       goto CLOSE_CONN;
-      continue;
     }
     file_name[file_len] = '\0';
 
@@ -108,18 +95,14 @@ int server(char *path, uint16_t port) {
         strstr(file_name, "..") != NULL) {
       fprintf(stderr, "invalid file name: %s\n", file_name);
       free(file_name);
-      // close(conn);
       goto CLOSE_CONN;
-      continue;
     }
 
     int dirfd = open(path, O_RDONLY | O_DIRECTORY);
     if (dirfd < 0) {
       perror("open(output_dir)");
       free(file_name);
-      // close(conn);
       goto CLOSE_CONN;
-      continue;
     }
 
     int out = openat(dirfd, file_name, O_CREAT | O_WRONLY | O_TRUNC, 0644);
@@ -127,9 +110,7 @@ int server(char *path, uint16_t port) {
     if (out < 0) {
       perror("open");
       free(file_name);
-      // close(conn);
       goto CLOSE_CONN;
-      continue;
     }
 
 
@@ -149,9 +130,10 @@ int server(char *path, uint16_t port) {
 CLOSE_CONN:
     close(conn);
   }
+  
+  return 0;
 
 CLOSE_SOCK:
   close(sock);
-  
-  return 0;
+  return 1;
 }
