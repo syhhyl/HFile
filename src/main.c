058157517f4544e8490eacfac2a17bf2f50e6217
syhhyl <syhhyl926@126.com>
2026-02-12 22:35:11 +0800

    update main

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@
 #include "stdint.h"
 #include "stdlib.h"
 #include "helper.h"
-
+#include "stdbool.h"
 
 typedef enum {
   server_mode,
@@ -17,9 +17,9 @@ typedef enum {
 
 state flag = init_mode;
 
-const char *ip = "127.0.0.1";
-uint16_t port = 9000;
-char *file_path = NULL;
+// const char *ip = "127.0.0.1";
+// uint16_t port = 9000;
+// char *file_path = NULL;
 
 static void usage(const char *argv0) {
   fprintf(stderr,
@@ -41,61 +41,124 @@ static int parse_port(const char *s, uint16_t *out) {
 }
 
 
+
+// int main(int argc, char **argv) {
+//   int opt;
+//   char *server_path = NULL;
+
+//   while ((opt = getopt(argc, argv, "s:c:i:p:h")) != -1) {
+//     if (opt == 's') {
+//       if (flag == client_mode) {
+//         fprintf(stderr, "cannot use -s and -c together\n");
+//         usage(argv[0]);
+//         return 1;
+//       }
+//       flag = server_mode;
+//       server_path = optarg;
+//     } else if (opt == 'c') {
+//       if (flag == server_mode) {
+//         fprintf(stderr, "cannot use -s and -c together\n");
+//         usage(argv[0]);
+//         return 1;
+//       }
+//       flag = client_mode;
+//       file_path = optarg;
+//     } else if (opt == 'i') {
+//       ip = optarg;
+//     } else if (opt == 'p') {
+//       if (parse_port(optarg, &port) != 0) {
+//         fprintf(stderr, "invalid port: %s\n", optarg)
+//         return 1;
+//       }
+//     } else if (opt == 'h') {
+//       usage(argv[0]);
+//       return 0;
+//     } else {
+//       usage(argv[0]);
+//       return 1;
+//     }
+//   }
+
+//   if (flag == server_mode) {
+//     if (server_path == NULL) {
+//       fprintf(stderr, "missing -s <server_path>\n");
+//       usage(argv[0]);
+//       return 1;
+//     }
+//     return server(server_path, port);
+//   }
+
+//   if (flag == client_mode) {
+//     if (file_path == NULL) {
+//       fprintf(stderr, "missing -c <file_path>\n");
+//       usage(argv[0]);
+//       return 1;
+//     }
+//     return client(file_path, ip, port);
+//   }
+
+//   usage(argv[0]);
+//   return 1;
+// }
+
+
+//TODO rewrite main logic
 int main(int argc, char **argv) {
   int opt;
-  char *server_path = NULL;
+  
+  state st = init_mode;
+  char *path = NULL;
+  char *ip = "127.0.0.1";
+  uint16_t port = 9000;
+  
 
   while ((opt = getopt(argc, argv, "s:c:i:p:h")) != -1) {
-    if (opt == 's') {
-      if (flag == client_mode) {
-        fprintf(stderr, "cannot use -s and -c together\n");
+    printf("opt:%c ", opt);
+    
+    switch (opt) {
+      case 's':
+        if (st == client_mode) {
+          fprintf(stderr, "cannot use -c and -s together\n");
+          goto BAD_USE;
+        }
+        st = server_mode;
+        path = optarg;
+        // if (checkpath(optarg)) path = optarg;
+        // else goto BAD_USE;
+        break;
+      case 'c':
+        if (st == server_mode) {
+          fprintf(stderr, "cannot use -s and -c together\n");
+          goto BAD_USE;
+        }
+        st = client_mode;
+        path = optarg;
+        // if (checkpath(optarg)) path = optarg;
+        // else goto BAD_USE;
+        break;
+      case 'i':
+        ip = optarg;
+        break;
+      case 'p':
+        if (parse_port(optarg, &port) != 0) {
+          fprintf(stderr, "invalid port: %s\n", optarg);
+          goto BAD_USE;
+        }
+        break;
+      case 'h':
         usage(argv[0]);
-        return 1;
-      }
-      flag = server_mode;
-      server_path = optarg;
-    } else if (opt == 'c') {
-      if (flag == server_mode) {
-        fprintf(stderr, "cannot use -s and -c together\n");
-        usage(argv[0]);
-        return 1;
-      }
-      flag = client_mode;
-      file_path = optarg;
-    } else if (opt == 'i') {
-      ip = optarg;
-    } else if (opt == 'p') {
-      if (parse_port(optarg, &port) != 0) {
-        fprintf(stderr, "invalid port: %s\n", optarg);
-        return 1;
-      }
-    } else if (opt == 'h') {
-      usage(argv[0]);
-      return 0;
-    } else {
-      usage(argv[0]);
-      return 1;
+        return 0;
     }
   }
 
-  if (flag == server_mode) {
-    if (server_path == NULL) {
-      fprintf(stderr, "missing -s <server_path>\n");
-      usage(argv[0]);
-      return 1;
-    }
-    return server(server_path, port);
-  }
+  if (st == server_mode)
+    return server(path, port);
+  else if (st == client_mode)
+    return client(path, ip, port);
+      
 
-  if (flag == client_mode) {
-    if (file_path == NULL) {
-      fprintf(stderr, "missing -c <file_path>\n");
-      usage(argv[0]);
-      return 1;
-    }
-    return client(file_path, ip, port);
-  }
-
+BAD_USE:
   usage(argv[0]);
   return 1;
+  
 }
