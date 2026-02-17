9550aeec4f71da418e18d018905a251d0fe5a2b3
syhhyl <syhhyl926@126.com>
2026-02-17 22:01:17 +0800

    fix main

--- src/main.c
+++ src/main.c
@@ -1,30 +1,28 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <getopt.h>
-#include <errno.h>
-#include "server.h"
 #include "client.h"
-#include "stdint.h"
-#include "stdlib.h"
 #include "helper.h"
-#include "stdbool.h"
-
-
+#include "server.h"
 
 int main(int argc, char **argv) {
-
   Opt opt;
-  parse_args(argc, argv, &opt);
+  parse_result_t res = parse_args(argc, argv, &opt);
 
-  if (opt.exit_code == 1) {
+  if (res == PARSE_HELP) {
+    usage(argv[0]);
+    return 0;
+  }
+
+  if (res != PARSE_OK) {
     usage(argv[0]);
     return 1;
   }
 
-  // DBG("path: %s port: %u", path, port);
-  if (opt.mode == server_mode)
+  if (opt.mode == server_mode) {
     return server(opt.path, opt.port);
-  else if (opt.mode == client_mode)
+  }
+  if (opt.mode == client_mode) {
     return client(opt.path, opt.ip, opt.port);
-      
+  }
+
+  usage(argv[0]);
+  return 1;
 }
