214e0c0a51328ffab232d6fd669cb720f4098c8c
syhhyl <syhhyl926@126.com>
2026-02-17 10:39:56 +0800

    remove while in main

--- src/main.c
+++ src/main.c
@@ -12,60 +12,6 @@
 
 
 int main(int argc, char **argv) {
-  // int opt;
-  
-  // Mode st = init_mode;
-  // char *path = NULL;
-  // char *ip = "127.0.0.1";
-  // uint16_t port = 9000;
-  
-  
-
-  // while ((opt = getopt(argc, argv, "s:c:i:p:h")) != -1) { 
-  //   switch (opt) {
-  //     case 's':
-  //       if (st == client_mode) {
-  //         fprintf(stderr, "cannot use -s -c together\n");
-  //         goto BAD_USE;
-  //       }
-  //       st = server_mode;
-  //       if (*optarg == '-') {
-  //         fprintf(stderr, "invalid server path\n");
-  //         goto BAD_USE;
-  //       }
-  //       path = optarg;
-  //       break;
-  //     case 'c':
-  //       if (st == server_mode) {
-  //         fprintf(stderr, "cannot use -s -c together\n");
-  //         goto BAD_USE;
-  //       }
-  //       st = client_mode;
-  //       if (*optarg == '-') {
-  //         fprintf(stderr, "invalid client path\n");
-  //         goto BAD_USE;
-  //       }
-  //       path = optarg;
-  //       break;
-  //     case 'i':
-  //       if (st == client_mode)
-  //         ip = optarg;
-  //       else {
-  //         fprintf(stderr, "server mode don't need ip\n");
-  //         goto BAD_USE;
-  //       }
-  //       break;
-  //     case 'p':
-  //       if (parse_port(optarg, &port) != 0) {
-  //         fprintf(stderr, "invalid port: %s\n", optarg);
-  //         goto BAD_USE;
-  //       }
-  //       break;
-  //     case 'h':
-  //       usage(argv[0]);
-  //       return 0;
-  //   }
-  // }
 
   Opt opt;
   parse_args(argc, argv, &opt);
@@ -81,7 +27,4 @@ int main(int argc, char **argv) {
   else if (opt.mode == client_mode)
     return client(opt.path, opt.ip, opt.port);
       
-  
-
-  
 }
