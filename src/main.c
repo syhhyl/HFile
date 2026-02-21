259275d99f5139839f580802e34a2d4196643735
syhhyl <syhhyl926@126.com>
2026-02-21 12:46:24 +0800

    add _WIN32 support

--- src/main.c
+++ src/main.c
@@ -1,26 +1,43 @@
 #include "helper.h"
 
 int main(int argc, char **argv) {
+
+#ifdef _WIN32
+  WSADATA wsa;
+  if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+    fprintf(stderr, "WSAStartup failed\n");
+    return 1;
+  }
+#endif
+
   Opt opt;
   parse_result_t res = parse_args(argc, argv, &opt);
 
   if (res == PARSE_HELP) {
     usage(argv[0]);
+#ifdef _WIN32
+    WSACleanup();
+#endif
     return 0;
   }
 
   if (res != PARSE_OK) {
     usage(argv[0]);
+#ifdef _WIN32
+    WSACleanup();
+#endif
     return 1;
   }
 
+  int ret = 1;
   if (opt.mode == server_mode) {
-    return server(opt.path, opt.port);
-  }
-  if (opt.mode == client_mode) {
-    return client(opt.path, opt.ip, opt.port);
-  }
+    ret = server(opt.path, opt.port);
+  } else if (opt.mode == client_mode) {
+    ret = client(opt.path, opt.ip, opt.port);
+  } else usage(argv[0]);
 
-  usage(argv[0]);
-  return 1;
+#ifdef _WIN32
+  WSACleanup();
+#endif
+  return ret;
 }
