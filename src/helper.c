621add0badb24a960efec25ee3938e628246812b
syhhyl <syhhyl926@126.com>
2026-03-09 14:32:24 +0800

    fix helper.*

--- src/helper.c
+++ src/helper.c
@@ -10,18 +10,6 @@
 #endif
 
 
-int get_file_name(const char **file_path, const char **file_name) {
-  if (*file_path == NULL) return 1;
-  char *tmp;
-#ifdef _WIN32
-  tmp = strrchr(*file_path, '\\');
-#else
-  tmp = strrchr(*file_path, '/');
-#endif
-  if (tmp == NULL) *file_name = *file_path;
-  else *file_name = tmp + 1;
-  return *file_name ? 0 : 1;
-}
 
 uint64_t now_ns() {
 #ifdef _WIN32
--- src/helper.h
+++ src/helper.h
@@ -4,17 +4,16 @@
 #include <stdint.h>
 
 
-#ifdef DEBUG
-#define DBG(fmt, ...) \
-  fprintf(stderr, "[DBG %s:%d] " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
-#else
-#define DBG(...) ((void)0)
-#endif
+// #ifdef DEBUG
+// #define DBG(fmt, ...) \
+//   fprintf(stderr, "[DBG %s:%d] " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+// #else
+// #define DBG(...) ((void)0)
+// #endif
 
 
 #define CHUNK_SIZE 1024 * 1024
 
-int get_file_name(const char **file_path, const char **file_name);
 void report_transfer_perf(const char *mode, int ok, double total_s,
                           double io_s, double net_s, uint64_t file_bytes,
                           uint64_t wire_bytes);
