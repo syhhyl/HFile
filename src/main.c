77b62c0342e280c85f08b498f2f6c331d86e90fa
syhhyl <syhhyl926@126.com>
2026-03-11 16:55:02 +0800

    fix bug

--- src/main.c
+++ src/main.c
@@ -4,31 +4,44 @@
 #include "client.h"
 
 int main(int argc, char **argv) {
-  
   net_init();
 
+  int ret = 1;
   Opt opt = {0};
   parse_result_t res = parse_args(argc, argv, &opt);
 
-  int ret;
 
   if (res == PARSE_HELP) {
     usage(argv[0]);
     net_cleanup();
-    ret = 0;
-  }
-
-  if (res != PARSE_OK) {
+    return 0;
+  } else if (res == PARSE_ERR) {
     usage(argv[0]);
     net_cleanup();
-    ret = 1;
+    return 1;
   }
 
   if (opt.mode == server_mode) {
-    ret = server(opt.path, opt.port, opt.perf);
+    server_opt_t ser_opt = {
+      .path = opt.path,
+      .port = opt.port,
+      .perf = opt.perf
+    };
+    ret = server(&ser_opt);
   } else if (opt.mode == client_mode) {
-    ret = client(opt.path, opt.ip, opt.port, opt.perf);
-  } else usage(argv[0]);
+    client_opt_t cli_opt = {
+      .path = opt.path,
+      .message = opt.message,
+      .ip = opt.ip,
+      .port = opt.port,
+      .perf = opt.perf,
+      .msg_type = opt.msg_type,
+      .msg_flags = opt.msg_flags
+    };
+    ret = client(&cli_opt);
+  } else {
+    usage(argv[0]);
+  }
 
   net_cleanup();
   return ret;
