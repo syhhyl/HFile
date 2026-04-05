9833546e4d4140b519ec69d75e1fbd9bf855a5d7
syhhyl <syhhyl926@126.com>
2026-04-05 12:23:07 +0800

    refactor(http): remove unused http-only server path

--- src/http.h
+++ src/http.h
@@ -4,7 +4,6 @@
 #include "cli.h"
 #include "net.h"
 
-int http_server(socket_t listener, const server_opt_t *ser_opt);
 int http_handle_connection(socket_t conn, const server_opt_t *ser_opt);
 
 #endif  // HF_HTTP_H
