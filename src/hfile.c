ff7f6c1be7a7f3ce47c8107c6c0dcd2aa2678b09
syhhyl <syhhyl926@126.com>
2026-05-15 18:03:06 +0800

    refactor(cli): inline command parsing

--- src/hfile.c
+++ src/hfile.c
@@ -7,200 +7,162 @@
 #include <string.h>
 
 typedef enum {
-  PARSE_OK,
-  PARSE_HELP,
-  PARSE_ERR
-} parse_result_t;
-
-typedef enum {
   MODE_RECV,
   MODE_SEND,
 } Mode;
 
-typedef struct {
-  Mode mode;
-  const char *path;
-  const char *ip;
-  uint16_t port;
-} Opt;
-
-static void usage(const char *argv0) {
-  fprintf(stderr,
-    "HFile - fast file transfer over LAN\n"
-    "\n"
-    "usage:\n"
-    "  %s recv [<dir>] [-p <port>]\n"
-    "  %s send <file> [-i <ip>] [-p <port>]\n"
-    "\n"
-    "options:\n"
-    "  -i <ip>    target node address\n"
-    "  -p <port>  port number (default 8888)\n"
-    "  -h         show this help\n"
-    "\n"
-    "examples:\n"
-    "  %s recv /tmp/receive\n"
-    "  %s send ./foo.txt\n"
-    "  %s send ./bar.bin -i 192.168.1.10 -p 7777\n",
-    argv0, argv0, argv0, argv0, argv0);
-}
-
-static int parse_port(const char *s, uint16_t *out) {
-  if (s == NULL || *s == '\0') return 1;
-  errno = 0;
-  char *end = NULL;
-  unsigned long v = strtoul(s, &end, 10);
-  if (errno != 0 || end == s || *end != '\0') return 1;
-  if (v == 0 || v > 65535UL) return 1;
-  *out = (uint16_t)v;
-  return 0;
-}
-
-static int need_value(int argc, char **argv, int *i, const char **out) {
-  if (*i + 1 >= argc) return 1;
-  *i = *i + 1;
-  *out = argv[*i];
-  return 0;
-}
-
-static int take_value(int argc, char **argv, int *i,
-                      const char *error_message, const char **out) {
-  if (need_value(argc, argv, i, out) != 0) {
-    fprintf(stderr, "%s\n", error_message);
-    return 1;
-  }
-  return 0;
-}
-
-static parse_result_t parse_args(int argc, char **argv, Opt *opt) {
-  if (opt == NULL) return PARSE_ERR;
-
-  opt->path = NULL;
-  opt->ip = NULL;
-  opt->port = 8888;
-  opt->mode = MODE_RECV;
-
+int main(int argc, char **argv) {
+  Mode mode = MODE_RECV;
+  const char *path = NULL;
+  const char *ip = NULL;
+  uint16_t port = 8888;
+  int show_usage = 0;
+  int parse_error = 0;
   int ip_seen = 0;
   int positional_seen = 0;
 
   if (argc < 2 || argv[1] == NULL) {
     fprintf(stderr, "missing command\n");
-    return PARSE_ERR;
+    parse_error = 1;
+    goto usage;
   }
 
   if (strcmp(argv[1], "-h") == 0) {
-    return PARSE_HELP;
+    show_usage = 1;
+    goto usage;
   }
 
   if (strcmp(argv[1], "recv") == 0) {
-    opt->mode = MODE_RECV;
+    mode = MODE_RECV;
   } else if (strcmp(argv[1], "send") == 0) {
-    opt->mode = MODE_SEND;
+    mode = MODE_SEND;
   } else {
     fprintf(stderr, "unknown command\n");
-    return PARSE_ERR;
+    parse_error = 1;
+    goto usage;
   }
 
   for (int i = 2; i < argc; i++) {
     const char *a = argv[i];
     if (a == NULL) {
       fprintf(stderr, "invalid argument\n");
-      return PARSE_ERR;
+      parse_error = 1;
+      goto usage;
     }
 
     if (a[0] != '-' || a[1] == '\0') {
       if (positional_seen) {
         fprintf(stderr, "unexpected extra argument\n");
-        return PARSE_ERR;
+        parse_error = 1;
+        goto usage;
       }
-      opt->path = a;
+      path = a;
       positional_seen = 1;
       continue;
     }
 
     if (a[1] == '-') {
       fprintf(stderr, "invalid argument\n");
-      return PARSE_ERR;
+      parse_error = 1;
+      goto usage;
     }
 
     if (a[2] != '\0') {
       fprintf(stderr, "invalid argument\n");
-      return PARSE_ERR;
+      parse_error = 1;
+      goto usage;
     }
 
     switch (a[1]) {
       case 'h':
-        return PARSE_HELP;
+        show_usage = 1;
+        goto usage;
 
       case 'i': {
-        const char *v = NULL;
-        if (opt->mode == MODE_RECV) {
+        if (mode == MODE_RECV) {
           fprintf(stderr, "recv mode does not accept -i\n");
-          return PARSE_ERR;
+          parse_error = 1;
+          goto usage;
         }
-        if (take_value(argc, argv, &i, "invalid address", &v) != 0) {
-          return PARSE_ERR;
+        if (i + 1 >= argc) {
+          fprintf(stderr, "invalid address\n");
+          parse_error = 1;
+          goto usage;
         }
-
-        opt->ip = v;
+        ip = argv[++i];
         ip_seen = 1;
         break;
       }
 
       case 'p': {
-        const char *v = NULL;
-        if (take_value(argc, argv, &i, "invalid port", &v) != 0) {
-          return PARSE_ERR;
-        }
-        if (parse_port(v, &opt->port) != 0) {
+        if (i + 1 >= argc) {
           fprintf(stderr, "invalid port\n");
-          return PARSE_ERR;
+          parse_error = 1;
+          goto usage;
         }
+        const char *s = argv[++i];
+        if (s == NULL || *s == '\0') {
+          fprintf(stderr, "invalid port\n");
+          parse_error = 1;
+          goto usage;
+        }
+        errno = 0;
+        char *end = NULL;
+        unsigned long v = strtoul(s, &end, 10);
+        if (errno != 0 || end == s || *end != '\0' ||
+            v == 0 || v > 65535UL) {
+          fprintf(stderr, "invalid port\n");
+          parse_error = 1;
+          goto usage;
+        }
+        port = (uint16_t)v;
         break;
       }
 
       default:
         fprintf(stderr, "invalid argument\n");
-        return PARSE_ERR;
+        parse_error = 1;
+        goto usage;
     }
   }
 
-  if (opt->mode == MODE_RECV && opt->path == NULL) {
-    opt->path = ".";
+  if (mode == MODE_RECV && path == NULL) {
+    path = ".";
   }
 
-  if (opt->mode == MODE_SEND && opt->path == NULL) {
+  if (mode == MODE_SEND && path == NULL) {
     fprintf(stderr, "missing file to send\n");
-    return PARSE_ERR;
+    parse_error = 1;
+    goto usage;
   }
 
-  if (ip_seen && opt->mode != MODE_SEND) {
+  if (ip_seen && mode != MODE_SEND) {
     fprintf(stderr, "recv mode does not accept -i\n");
-    return PARSE_ERR;
+    parse_error = 1;
+    goto usage;
   }
 
-  return PARSE_OK;
-}
+  return mode == MODE_RECV ? node_recv(path, port) : node_send(path, ip, port);
 
-int main(int argc, char **argv) {
-  int ret = 1;
-
-  Opt opt = {0};
-  parse_result_t res = parse_args(argc, argv, &opt);
-
-  if (res == PARSE_HELP || res == PARSE_ERR) {
-    usage(argv[0]);
-    ret = (res == PARSE_HELP) ? 0 : 1;
-    return ret;
+usage:
+  if (show_usage || parse_error) {
+    fprintf(stderr,
+      "HFile - fast file transfer over LAN\n"
+      "\n"
+      "usage:\n"
+      "  %s recv [<dir>] [-p <port>]\n"
+      "  %s send <file> [-i <ip>] [-p <port>]\n"
+      "\n"
+      "options:\n"
+      "  -i <ip>    target node address\n"
+      "  -p <port>  port number (default 8888)\n"
+      "  -h         show this help\n"
+      "\n"
+      "examples:\n"
+      "  %s recv /tmp/receive\n"
+      "  %s send ./foo.txt\n"
+      "  %s send ./bar.bin -i 192.168.1.10 -p 7777\n",
+      argv[0], argv[0], argv[0], argv[0], argv[0]);
   }
-
-  if (opt.mode == MODE_RECV) {
-    ret = node_recv(opt.path, opt.port);
-  } else if (opt.mode == MODE_SEND) {
-    ret = node_send(opt.path, opt.ip, opt.port);
-  } else {
-    usage(argv[0]);
-    ret = 1;
-  }
-
-  return ret;
+  return show_usage ? 0 : 1;
 }
