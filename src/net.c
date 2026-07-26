23acfefb83fd3afe1c8522ec16b205a1c5700b99
syhhyl <syhhyl926@126.com>
2026-07-26 22:40:35 +0800

    simplify socket transfer loops

--- src/net.c
+++ src/net.c
@@ -10,40 +10,42 @@ bool is_socket_invalid(socket_t sock) {
 }
 
 ssize_t send_all(socket_t sock, const void *data, size_t len) {
-  size_t total = 0;
   const char *p = data;
+  size_t remaining = len;
 
   int flags = 0;
   #ifdef MSG_NOSIGNAL
     flags = MSG_NOSIGNAL;
   #endif
 
-  while (total < len) {
-    ssize_t n = send(sock, p + total, len - total, flags);
+  while (remaining > 0) {
+    ssize_t n = send(sock, p, remaining, flags);
     if (n < 0) {
       if (errno == EINTR) continue;
       return -1;
     }
-    if (n == 0) return (ssize_t)total;
-    total += (size_t)n;
+    if (n == 0) break;
+    p += n;
+    remaining -= (size_t)n;
   }
-  return (ssize_t)total;
+  return (ssize_t)(len - remaining);
 }
 
 ssize_t recv_all(socket_t sock, void *buf, size_t len) {
   uint8_t *p = (uint8_t *)buf;
-  size_t total = 0;
+  size_t remaining = len;
 
-  while (total < len) {
-    ssize_t n = recv(sock, p + total, len - total, 0);
+  while (remaining > 0) {
+    ssize_t n = recv(sock, p, remaining, 0);
     if (n < 0) {
       if (errno == EINTR) continue;
       return -1;
     }
-    if (n == 0) return (ssize_t)total;
-    total += (size_t)n;
+    if (n == 0) break;
+    p += n;
+    remaining -= (size_t)n;
   }
-  return (ssize_t)total;
+  return (ssize_t)(len - remaining);
 }
 
 int socket_close(socket_t s) {
